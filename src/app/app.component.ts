46dbb9b6bdc52011b6bce857bf16411c93c20353
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-30 17:52:53 -0300

    chore: added observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'rxjs-course';
+
+  constructor() {
+    const observable$ = new Observable(observer => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.next(4);
+      observer.complete();
+    });
+
+    observable$.subscribe(
+      v => console.log(v),
+      err => console.log(err),
+      () => console.log('this is the end'),
+    );
+  }
 }
