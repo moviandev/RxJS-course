46a697fb56f8373460e5fa5b6e3e92042175a9e4
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-30 18:20:34 -0300

    chore: added Subject and added Behaviorsubject on the top of it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy {
   title = 'rxjs-course';
   observable$;
+  subject$: Subject<number>;
 
   ngOnInit() {
     this.observable$ = new Observable<any>(observer => {
@@ -23,9 +24,19 @@ export class AppComponent implements OnInit, OnDestroy {
       err => console.log(err),
       () => console.log('this is the end'),
     );
+
+    this.subject$ = new Subject<number>();
+    this.subject$.subscribe(x => console.log('First subscribe ==> ', x));
+    this.subject$.next(1);
+    this.subject$.next(2);
+    this.subject$.next(3);
+
+    this.subject$.subscribe(v => console.log('second subscribe ==> ', v));
+    this.subject$.next(3);
   }
 
   ngOnDestroy() {
     this.observable$.unsubscribe();
+    this.subject$.unsubscribe();
   }
 }
